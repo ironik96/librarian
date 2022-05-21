4f2de5f84fcc37d46940887cf064e7296c81e0b4
Ahmad AlAmiri <a.alamiri.96@gmail.com>
2022-05-21 20:16:02 +0300

    Add navigation buttons to switch pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import Navigator from "./components/Navigator";
 import BooksList from "./components/BooksList";
 import Actions from "./components/Actions";
+import { Route, Routes } from "react-router-dom";
+import MembersPage from "./components/MembersPage";
 
 function App() {
   return (
@@ -10,8 +12,18 @@ function App() {
         <div className="container">
           <Navigator />
           <div className="main-page">
-            <Actions />
-            <BooksList />
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <>
+                    <Actions />
+                    <BooksList />
+                  </>
+                }
+              />
+              <Route path="/members" element={<MembersPage />} />
+            </Routes>
           </div>
         </div>
       </header>
--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -1,5 +1,26 @@
+import { NavLink } from "react-router-dom";
+
 const Navigator = () => {
-  return <div>Navigator</div>;
+  return (
+    <nav className="side-nav">
+      <NavLink className="nav-btn" to="/">
+        <img
+          className="nav-btn-icon"
+          src="icons/books.png"
+          alt="books button"
+        />
+        Books
+      </NavLink>
+      <NavLink className="nav-btn" to="/members">
+        <img
+          className="nav-btn-icon"
+          src="icons/members.png"
+          alt="members button"
+        />
+        Members
+      </NavLink>
+    </nav>
+  );
 };
 
 export default Navigator;
