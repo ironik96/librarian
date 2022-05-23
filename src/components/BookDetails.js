25a73737166cc4e16b51f502907860795f1f9fb3
Ahmed AlAmiri <a.alamiri.96@gmail.com>
2022-05-23 16:38:29 +0300

    halfway through return book

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -11,9 +11,10 @@ const BookDetails = () => {
   const { bookSlug } = useParams();
   const book = booksStore.books.find((element) => element.slug === bookSlug);
   const members = membersStore.members;
-  const [borrowerID, setBorrower] = useState(members[0]._id);
-
+  const [borrowerID, setBorrower] = useState(members[0]?._id);
   if (!book) return <p>Loading ...</p>;
+
+  const selectorClass = book.available ? "" : "hide-select";
 
   const borrowedBy = members
     .filter((member) => book.borrowedBy.includes(member._id))
@@ -45,6 +46,24 @@ const BookDetails = () => {
       booksStore.modifyBooksBorrowedList(book._id, wantsToBorrow._id);
     }
   };
+  const handleReturn = () => {
+    const wantsToBorrow = members.find((member) => member._id === borrowerID);
+    const numOfBorrowedBooks = wantsToBorrow.currentlyBorrowedBooks.length;
+    const numOfAllowedBooks = NUMBER_OF_ALLOWED_BOOKS[wantsToBorrow.membership];
+
+    if (numOfBorrowedBooks === numOfAllowedBooks) {
+      console.log("not allowed to borrow");
+    } else {
+      console.log("allowed to borrow");
+      booksStore.modifyBooksBorrowedList(book._id, wantsToBorrow._id);
+    }
+  };
+
+  const button = book.available ? (
+    <BorrowButton handleBorrow={handleBorrow} />
+  ) : (
+    <ReturnButton handleReturn={handleReturn} />
+  );
 
   return (
     <div className="book-details-container">
@@ -58,18 +77,28 @@ const BookDetails = () => {
         </div>
         <div className="borrowers">borrowed by: {borrowedBy}</div>
         <div className="to-borrow">
-          <select onChange={handleChange}>{membersNames}</select>
-          <button
-            onClick={handleBorrow}
-            className="add-borrower"
-            name="fullName"
-          >
-            Add a borrower
-          </button>
+          <select className={selectorClass} onChange={handleChange}>
+            {membersNames}
+          </select>
+          {button}
         </div>
       </div>
     </div>
   );
 };
 
+const BorrowButton = ({ handleBorrow }) => {
+  return (
+    <button onClick={handleBorrow} className="add-borrower" name="fullName">
+      Add a borrower
+    </button>
+  );
+};
+const ReturnButton = ({ handleReturn }) => {
+  return (
+    <button onClick={handleReturn} className="add-borrower" name="fullName">
+      Return Book
+    </button>
+  );
+};
 export default observer(BookDetails);
